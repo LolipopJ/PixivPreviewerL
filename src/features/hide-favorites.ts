e7ce6155018c001e76ff87ad2f543731f28b8197
JasonSung <42314340+LolipopJ@users.noreply.github.com>
2026-02-24 23:48:24 +0800

    fix(hide-favorite): fix behavior in search page

--- src/features/hide-favorites.ts
+++ src/features/hide-favorites.ts
@@ -6,7 +6,16 @@ const hideFavorites = () => {
     return $(this).css("color") === "rgb(255, 64, 96)";
   });
   favoriteSvgs.each(function () {
-    const listItem = $(this).closest("li");
+    // 兼容常规页面和搜索页 https://www.pixiv.net/search?q=*&s_mode=tag&type=illust_ugoira
+    const listItem: JQuery<HTMLElement> = $(this).closest("li, div.col-span-2");
+    // if (listItem.length === 0) {
+    //   // 兼容主页关注作者新作品 https://www.pixiv.net/illustration
+    //   // 由于此处左右滚动时会动态加载内容，隐藏后会产生预期外现象，故关闭此功能
+    //   const listDom = $(this).closest("ol, ul");
+    //   if (listDom.length > 0) {
+    //     listItem = listDom.children().has($(this));
+    //   }
+    // }
     listItem.hide();
     listItem.attr("data-pp-fav-hidden", "true");
   });
