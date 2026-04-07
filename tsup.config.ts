78f3071a47aae4fe77d019a0858d10ceed9fcfc3
Lolipop <42314340+LolipopJ@users.noreply.github.com>
2026-04-07 10:42:28 +0800

    chore: upgrade deps
    Wow jquery 4.0 is released?!

--- tsup.config.ts
+++ tsup.config.ts
@@ -39,7 +39,8 @@ export default defineConfig({
 // @icon                https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&size=32&url=https://www.pixiv.net
 // @icon64              https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&size=64&url=https://www.pixiv.net
 // @require             https://update.greasyfork.org/scripts/515994/1478507/gh_2215_make_GM_xhr_more_parallel_again.js
-// @require             http://code.jquery.com/jquery-3.7.1.min.js
+// @require             https://code.jquery.com/jquery-4.0.0.min.js
+// @require             https://code.jquery.com/jquery-migrate-4.0.2.min.js
 // @run-at              document-end
 // ==/UserScript==`,
   },
